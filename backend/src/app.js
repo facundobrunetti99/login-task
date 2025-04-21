ba6006f6d8eb0c95d6ce425ce0a5cfd4bf8267df
Facundo Brunetti <facundobrunetti99@gmail.com>
2025-04-21 07:49:50 -0300

    cors +

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,13 @@ import morgan from "morgan";
 import authRoutes from "../src/routers/auth.routes.js"
 import cookieParser from 'cookie-parser'
 import taskRoutes from "../src/routers/task.routes.js"
+import cors from "cors";
+
 
 const app = express();
+app.use(cors({
+    origin:'http://localhost:5173'
+}))
 app.use(morgan('dev'));
 app.use(express.json())
 app.use(cookieParser())
