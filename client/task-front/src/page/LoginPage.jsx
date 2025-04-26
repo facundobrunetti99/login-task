65de1df8d034a6d107bbb71c3d4d118088aa3bd5
Facundo Brunetti <facundobrunetti99@gmail.com>
2025-04-25 21:30:19 -0300

    login page added

--- client/task-front/src/page/LoginPage.jsx
+++ client/task-front/src/page/LoginPage.jsx
@@ -2,18 +2,45 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 
 const LoginPage = () => {
-    const {register,handleSubmit}=useForm();
+   const {register, handleSubmit,formState:{errors}}=useForm();
+   const onSubmit=handleSubmit(data =>{
+    console.log(data)
+   })
     
   return (
-    <div>
-        <form onSubmit={handleSubmit((values)=>{console.log(values)})}>
-        <input type="text" {...register("username",{required:true})} />
+    <div className='flex h-[calc(100vh-100px)] items-center justify-center'>
+    <div className='bg-zinc-800 max-w-md w-full p-10 rounded-md'>
+    <form onSubmit={onSubmit}>
+    
+    <input
+      type="text"
+      {...register("email", { required: true })}
+      className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+      placeholder="Ingrese su email"
+    />
+      {
+      errors.email &&(
+        <p className="text-red-500">El mail es requerido</p>
+      )
+    }
 
-        <input type="text" {...register("email",{required:true})} />
-        <input type="password" {...register("password",{required:true})}/>    
-        <button type="submit">Registrar</button>
-        </form>
-        </div>
+    <input
+      type="password"
+      {...register("password", { required: true })}
+      className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+      placeholder="Ingrese su contraseña"
+    />
+      {
+      errors.password &&(
+        <p className="text-red-500">La contraseña es requerida</p>
+      )
+    }
+
+    <button type="submit">Registrar</button>
+  </form>
+    </div>
+
+    </div>
   )
 }
 
